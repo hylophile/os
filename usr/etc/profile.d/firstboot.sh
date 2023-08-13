594681126301d1a9789aea92dae585d02b23278e
Ellis Kenyo <me@elken.dev>
2023-08-13 20:44:17 +0100

    feat: first attempt at firstboot setup

--- usr/etc/profile.d/firstboot.sh
+++ usr/etc/profile.d/firstboot.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 #
 if test "$(id -u)" -gt "0" && test -d "$HOME" && test ! -e "$HOME/.config/.bootstrap-done"; then
-    foot -e zsh
+    echo "exec foot -e yadm clone https://github.com/elken/.files" >"$HOME/.config/sway/config.d/99-firstboot.conf"
 fi
