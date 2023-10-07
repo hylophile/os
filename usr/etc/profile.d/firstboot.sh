44fb2544ce715747bae634856852b23445322f77
hylo <hylo@posteo.de>
2023-10-07 12:51:08 +0200

    fix: attempt to fix firstboot

--- usr/etc/profile.d/firstboot.sh
+++ usr/etc/profile.d/firstboot.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 #
 if test "$(id -u)" -gt "0" && test -d "$HOME" && test ! -e "$HOME/.config/.bootstrap-done"; then
+    mkdir -p "$HOME/.config/sway/config.d"
     echo "exec foot -e yadm clone https://github.com/elken/.files" >"$HOME/.config/sway/config.d/99-firstboot.conf"
 fi
